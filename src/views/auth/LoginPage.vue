420573554f63ec1e3e696fab2d24a64fd14f9049
Kwasu <lukaszkwas96@gmail.com>
2024-07-25 15:52:54 +0200

    LoginPage

--- src/views/auth/LoginPage.vue
+++ src/views/auth/LoginPage.vue
@@ -7,23 +7,11 @@
 
       <div class="inputs">
         <div class="group" id="email">
-          <svg class="icon" aria-hidden="true" viewBox="0 0 24 24">
-            <g>
-              <path
-                d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
-              ></path>
-            </g>
-          </svg>
+          <i class="fa-regular fa-envelope"></i>
           <input placeholder="Email" type="email" class="input" />
         </div>
         <div class="group" id="password">
-          <svg class="icon" aria-hidden="true" viewBox="0 0 24 24">
-            <g>
-              <path
-                d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"
-              ></path>
-            </g>
-          </svg>
+          <i class="fa-solid fa-lock"></i>
           <input placeholder="Password" type="password" class="input" />
         </div>
       </div>
@@ -134,10 +122,10 @@ export default {
         background-color: #f5f6fa;
       }
     }
-    .icon {
+    i {
       position: absolute;
       left: 1rem;
-      fill: #9e9ea7;
+      color: #1e2438;
       width: 1rem;
       height: 1rem;
     }
